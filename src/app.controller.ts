048291ef2f8eaee899cfa3debb8e8fb2fcabafa8
Ram Baldeo <rambaldeo241@gmail.com>
2025-10-23 12:23:48 -0400

    Instralled NestJs/Swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Render, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
-
+//Need to adjust this to show the home page of the application
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
